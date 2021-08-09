b42b8edc6be0b1794454eb17116e62f9fc83b632
amitkumardube <amitkumardube@gmail.com>
2021-08-09 10:16:27 +0300

    Added barrels file

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// creating barrel to put all the exports together
+
+// creating readJson as export alias
+//import * as readJson from './readfile';
+
+// creating createMessage as export alias
+//import * as createMessage from './markdown';
+
+// exporting all the imports using alias using export statement
+
+export * from './readfile';
+export * from './markdown';
+//export { readJson, createMessage };
