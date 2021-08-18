acde732d24bc394db57f351abfd4591e6771f515
amitkumardube <amitkumardube@gmail.com>
2021-08-18 22:30:58 +0300

    updated code to handle the promise

--- dist/index.js
+++ dist/index.js
@@ -6,25 +6,6 @@
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -39,8 +20,6 @@ exports.commitValidation = void 0;
 // need github library to access information about github context
 // javascript style
 const github = __nccwpck_require__(438);
-// typescript style
-const core = __importStar(__nccwpck_require__(186));
 // checking for commit message in the git commit history
 // for the moment we are only checking the head commit.
 function commitValidation() {
@@ -54,16 +33,16 @@ function commitValidation() {
             // initiating the regular expression constructor
             let commitmsgpattern = new RegExp('^\[[a-z]+-[0-9]+\]', 'i');
             if (commitmsgpattern.test(commitmsg) && commitmsg_length >= 20) {
-                core.info("Commit Message is valid");
+                //core.info("Commit Message is valid");
                 return "Commit message is valid";
             }
             else {
-                core.setFailed("Commit Message should always start with Reference number in the format as [WWN-1234] and commit message should be >= 20 characters");
+                //core.setFailed("Commit Message should always start with Reference number in the format as [WWN-1234] and commit message should be >= 20 characters");
                 return "Commit Message should always start with Reference number in the format as [WWN-1234] and commit message should be >= 20 characters";
             }
         }
         else {
-            core.info("Can only run on push to a branch");
+            //core.info("Can only run on push to a branch");
             return "Can only run on push to a branch";
         }
     });
@@ -199,9 +178,13 @@ function run() {
     });
 }
 // Our main method: call the run() function and report any errors
-run().catch(error => core.setFailed("Workflow failed! " + error.message));
+run().
+    then(res => core.info(res))
+    .catch(error => core.setFailed("Workflow failed! " + error.message));
 // calling the commitMsg function to get the commit message from last commit
-_1.commitValidation().catch(error => core.setFailed("Can't get the commit message" + error.message));
+_1.commitValidation()
+    .then(res => core.info(res))
+    .catch(error => core.setFailed("Can't get the commit message" + error.message));
 //# sourceMappingURL=main.js.map
 
 /***/ }),
